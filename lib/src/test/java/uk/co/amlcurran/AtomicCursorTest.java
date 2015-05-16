6d9b40ac0dfc3b5127e8e60897edea0387ec7c43
Alex Curran <aml.curran@gmail.com>
2015-05-16 09:31:10 +0100

    Make library an Android library

--- lib/src/test/java/uk/co/amlcurran/AtomicCursorTest.java
+++ lib/src/test/java/uk/co/amlcurran/AtomicCursorTest.java
@@ -2,13 +2,11 @@ package uk.co.amlcurran;
 
 import org.junit.Test;
 
-import static org.fest.assertions.api.Assertions.assertThat;
-
 public class AtomicCursorTest {
 
     @Test
-    public void testOneIsOne() {
-        assertThat(1 + 1).isEqualTo(2);
+    public void testSubmittingACursorResultsInDatasetChanged() {
+
     }
 
 }
